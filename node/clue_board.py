58a9af9118ca06842ec100ea814954239cab263c
lisas7z <lisashi714@gmail.com>
2024-04-02 16:32:11 -0700

    CNN ready

--- node/clue_board.py
+++ node/clue_board.py
@@ -6,8 +6,6 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 from std_msgs.msg import String
-from line_following import Imag_Convert
-from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Clueboard_detection:
 
@@ -20,6 +18,7 @@ class Clueboard_detection:
         self.board = False
         self.frame_counter = 1
         self.blur = []
+        self.board_count = 0
 
         # img = cv2.imread("/home/fizzer/ros_ws/src/Zoo-Wee-Mama/00.png")
         # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -28,7 +27,7 @@ class Clueboard_detection:
         
     def hsv_callback(self, data):
         #variables
-        lower_blue = np.array([90, 50, 50])
+        lower_blue = np.array([100, 50, 50])
         upper_blue = np.array([130, 255, 255])
 
         # edge_blue = np.array([120, 255, 102])
@@ -102,19 +101,18 @@ class Clueboard_detection:
             # Perspective transform
             h, w, _ = img.shape
             pts = np.float32([[0, 0], [0, h], [w, h], [w, 0]]).reshape(-1, 1, 2)
+
             dst = cv2.perspectiveTransform(pts, matrix)
 
             homography = cv2.polylines(frame, [np.int32(dst)], True, (255, 0, 0), 3)
-            # cv2.imshow("Homography", homography)
-            # cv2.waitKey(2)
-
-            print(dst)
+            cv2.imshow("Homography", homography)
+            cv2.waitKey(2)
 
            # Get the bounding box of the transformed points
             min_x = max(int(np.min(dst[:, 0, 0])), 0)
             min_y = max(int(np.min(dst[:, 0, 1])), 0)
-            max_x = min(int(np.max(dst[:, 0, 0])), frame.shape[1])
-            max_y = min(int(np.max(dst[:, 0, 1])), frame.shape[0])
+            max_x = min(int(np.max(dst[:, 0, 0])), w)
+            max_y = min(int(np.max(dst[:, 0, 1])), h)
 
             # Crop the region from the homography image based on the adjusted coordinates
             cropped_roi = self.blur[min_y:max_y, min_x:max_x]
@@ -124,17 +122,59 @@ class Clueboard_detection:
             cv2.imwrite("cropped_roi.jpg", cropped_roi)
 
             # Display the cropped ROI (optional)
-            cv2.imshow("Cropped ROI", cropped_roi)
-            cv2.waitKey(2)
+            # cv2.imshow("Cropped ROI", cropped_roi)
+            # cv2.waitKey(2)
 
-            self.character_trim(cropped_roi)
+            self.words_trim(cropped_roi)
+            self.board_count +=1
 
         except Exception as e:
-            rospy.logerr("Homography error: %s", str(e))
-            homography = frame
+            # rospy.logerr("Homography error: %s", str(e))
+            # homography = frame
+            pass
             
-    def character_trim(self, cropped):
-        pass
+    def words_trim(self, cropped):
+        
+
+        contours, _ = cv2.findContours(cropped, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        
+        trim_img = cropped.copy()
+        iteration = 0
+
+        for contour in contours:
+            
+            x, y, w, h = cv2.boundingRect(contour)
+            
+            if w<650 and w>55 and h<650 and h>55:
+                cv2.rectangle(trim_img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+                cv2.imshow("contour", trim_img)
+                cv2.waitKey(2)
+
+                string_img = trim_img[y:y+h, x:x+w]
+                self.character_trim(string_img, iteration)
+                iteration += 1
+                # cv2.imwrite("Characters.jpg", character)
+                # cv2.imshow("Character", string_img)
+                # cv2.waitKey(2)
+
+    def character_trim(self, string_img, iteration):
+        h, w = string_img.shape
+        space = 80
+        folder_path = "/home/fizzer/ros_ws/src/Zoo-Wee-Mama/Characters/"
+
+        if int(w//space) == 0:
+            num = 1
+        else:
+            num = int(w//space)
+        
+        for character in range(num):
+            character_img = string_img[0:h, space * character : space * (character+1)]
+            file_name = str(self.board_count) + str(iteration) + str(character) + '.jpg'
+            full_path = folder_path + file_name
+            cv2.imwrite(full_path, character_img)
+
+            # cv2.imshow("char", character_img)
+            # cv2.waitKey(2)
 
 
 def main():
